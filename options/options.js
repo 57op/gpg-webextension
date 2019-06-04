2af706413f1587096f11e390604ebdc937bcd252
chuck- <dovah@tuta.io>
2019-06-04 15:13:06 +0200

    fix indentation inconsistencies

--- options/options.js
+++ options/options.js
@@ -1,61 +1,61 @@
 function makeLi(url) {
-	const li = document.createElement('li')
-	const span = document.createElement('span')
-	const btn = document.createElement('button')
+  const li = document.createElement('li')
+  const span = document.createElement('span')
+  const btn = document.createElement('button')
 
-	span.appendChild(document.createTextNode(url))
-	btn.appendChild(document.createTextNode('X'))
-	li.appendChild(span)
-	li.appendChild(btn)
+  span.appendChild(document.createTextNode(url))
+  btn.appendChild(document.createTextNode('X'))
+  li.appendChild(span)
+  li.appendChild(btn)
 
-	btn.addEventListener('click', removeLi)
+  btn.addEventListener('click', removeLi)
 
-	return li
+  return li
 }
 
 function removeLi(e) {
-	e.target.parentNode.remove()
-	saveOptions(e)
+  e.target.parentNode.remove()
+  saveOptions(e)
 }
 
 function saveOptions(e) {
-	e.preventDefault()
+  e.preventDefault()
 
-	const url = document.querySelector('#url')
+  const url = document.querySelector('#url')
 
-	if (url.value !== '') {
-		document.querySelector('#url-list').appendChild( makeLi(url.value) )
-		url.value = '';
-	}
+  if (url.value !== '') {
+    document.querySelector('#url-list').appendChild( makeLi(url.value) )
+    url.value = '';
+  }
 
-	const urls = Array
-		.from(document.querySelectorAll('#url-list li:not(:first-child) span'))
-		.map(li => li.textContent);
+  const urls = Array
+    .from(document.querySelectorAll('#url-list li:not(:first-child) span'))
+    .map(li => li.textContent);
 
-	browser.storage.local.set({
-		urls: urls
-	})
+  browser.storage.local.set({
+    urls: urls
+  })
 
 }
 
 function restoreOptions() {
-	function updateList(result) {
-		if (!result.urls) {
-			return
-		}
+  function updateList(result) {
+    if (!result.urls) {
+      return
+    }
 
-		for (const url of result.urls) {
-			urlList.appendChild(makeLi(url));
-		}
-	}
+    for (const url of result.urls) {
+      urlList.appendChild(makeLi(url));
+    }
+  }
 
-	function onError(err) {
-		console.log(`Error: ${err}`)
-	}
+  function onError(err) {
+    console.log(`Error: ${err}`)
+  }
 
-	const urlList = document.querySelector('#url-list')
-	const getUrls = browser.storage.local.get('urls')
-	getUrls.then(updateList, onError)
+  const urlList = document.querySelector('#url-list')
+  const getUrls = browser.storage.local.get('urls')
+  getUrls.then(updateList, onError)
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions)
